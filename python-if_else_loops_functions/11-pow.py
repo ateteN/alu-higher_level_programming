08354ec6fc1ba406e178f39e7744c6d34b84c9c3
ateteN <n.atete@alustudent.com>
2024-10-25 01:15:19 -0700

    Tasks

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b < 0:
-        result = 1 / a
-        for _ in range(abs(b) - 1):
-            result /= a
-    else:
-        result = 1
-        for _ in range(b):
-            result *= a
-    return result
+    return a ** b
