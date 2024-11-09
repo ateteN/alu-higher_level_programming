c9bcda14afd24d8daf5252cf85daa16918d4cda5
ateteN <n.atete@alustudent.com>
2024-11-08 23:55:21 -0800

    python-classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+The class currently does not have any methods or attributes.
+"""
+
+
+class Square:
+    """A class representing a square."""
+    pass
