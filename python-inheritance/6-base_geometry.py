b692ada7c9397c42291f3529347572f5b23e190a
ateteN <n.atete@alustudent.com>
2024-11-09 01:09:58 -0800

    python-inheritance

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""
-Module: 6-base_geometry
-Public instance method: def area(self): that raises an Exception with the message area() is not implemented
-"""
+""" class BaseGeometry"""
 
 
 class BaseGeometry:
-    """
-    It raises an Exception with the message area() is not implemented
-    """
-
+    """ base class BaseGeometry"""
     def area(self):
         raise Exception("area() is not implemented")
