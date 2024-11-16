d92b192b4488eaccb5356d3dd8f88a43b2d5a569
ateteN <n.atete@alustudent.com>
2024-11-16 01:30:46 -0800

    input-output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-
-
+"""Add arguments to a Python list and save them to a file."""
 import sys
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-<<<<<<< Updated upstream
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-=======
-  save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-  load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
->>>>>>> Stashed changes
-
-  try:
-        items = load_from_json_file("add_item.json")
-  except FileNotFoundError:
-        items = []
-<<<<<<< Updated upstream
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
-=======
-  items.extend(sys.argv[1:])
-  save_to_json_file(items, "add_item.json")
->>>>>>> Stashed changes
+    args = sys.argv[1:]
+    try:
+        data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        data = []
+    data = data + args
+    save_to_json_file(data, "add_item.json")
