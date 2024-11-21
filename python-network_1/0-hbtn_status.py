eb8d67710ea98b22c02c3169bc2ad162d3daa01b
ateteN <n.atete@alustudent.com>
2024-11-21 09:26:09 -0800

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""
-A script that fetches https://alu-intranet.hbtn.io/status using urllib.
-The script displays
-"""
-
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "https://alu-intranet.hbtn.io/status" 
 
     with urllib.request.urlopen(url) as response:
         body = response.read()
@@ -15,3 +10,4 @@ if __name__ == "__main__":
         print("\t- type:", type(body))
         print("\t- content:", body)
         print("\t- utf8 content:", body.decode("utf-8"))
+
