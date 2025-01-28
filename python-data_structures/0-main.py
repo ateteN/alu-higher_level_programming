05adfb7cd535a8e78ab4ceb78b53797fa172c86c
ateteN <n.atete@alustudent.com>
2025-01-28 01:37:06 -0800

    javascript-warm_up

--- python-data_structures/0-main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-list_module = __import__('0-print_list_integer')
-my_list = [1, 2, 3]
-result = list_module.print_list_integer(my_list)
--- python-inheritance/6-base_geometry.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" class BaseGeometry"""
-
-
-class BaseGeometry:
-    """ base class BaseGeometry"""
-    def area(self):
-        raise Exception("area() is not implemented")
